0f662cdb3ec71fb3439a97ba7b50eb90b4468f72
RyomaK <ryooomaaa0413@outlook.com>
2016-12-07 00:24:52 +0900

    addaaa

--- nissy.rb
+++ nissy.rb
@@ -13,35 +13,54 @@ ATC = ENV["T_ATC"]
 class Tweet 
 
 	def initialize(a,b,c,d)
-  	 @consumer_key        = ""
-  	 @consumer_secret     = ""
-  	 @access_token        = ""
-  	 @access_token_secret = ""
-  	 @client = Twitter::REST::Client.new(
-  	  consumer_key:        a,
-  	  consumer_secret:     b,
-  	  access_token:        c,
-  	  access_token_secret: d
-  	 )
+		@consumer_key        = ""
+		@consumer_secret     = ""
+		@access_token        = ""
+		@access_token_secret = ""
+		@client = Twitter::REST::Client.new(
+			consumer_key:        a,
+			consumer_secret:     b,
+			access_token:        c,
+			access_token_secret: d
+			)
 	end
 
 
-	def setData(n)
-		doc = Nokogiri::HTML(open('http://avex.jp/nissy/news/'))
-		time = doc.xpath('//time')
-		info = doc.xpath('//dd')
-		
-  	    @txt = ""
-		@txt += time[n].inner_text
-		@txt += "\n"
-		@txt += info[n].inner_text
-		@txt += "http://avex.jp/nissy/news/ #nissy"
-		ram = Rand.new()
-		@txt += " " * ram.getRand
-		if @txt.length <= 140 then
-			@txt.slice(/http+/,0)
-		elsif @txt.length >=140 then
-			@txt.slice (141..@txt.length)
+	def setData(n,k)
+		doc_nissy = Nokogiri::HTML(open('http://avex.jp/nissy/news/'))
+		doc_aaa = Nokogiri::HTML(open('http://avex.jp/aaa/news/'))
+		@txt = ""
+		if k == "nissy" then
+			time = doc_nissy.xpath('//time')
+			info = doc_nissy.xpath('//dd')
+
+			
+			@txt += time[n].inner_text
+			@txt += "\n"
+			@txt += info[n].inner_text
+			@txt += "http://avex.jp/nissy/news/#{info[n].css('a')[0][:href].gsub(/.\//,"")} ##{k}"
+			ram = Rand.new()
+			if @txt.length <= 140 then
+				
+			elsif @txt.length >=140 then
+				@txt.gsub(140..@txt.length,"")
+			end
+
+		elsif k =="AAA" then
+			time = doc_aaa.xpath('//dt')
+			info = doc_aaa.xpath('//dd')
+
+			@txt += time[n].inner_text
+			@txt += "\n"
+			@txt += info[n].inner_text.gsub(/newUp.+/,"\n")
+			@txt += " http://avex.jp/aaa/news/#{info[n].css('a')[0][:href]} ##{k}"
+			ram = Rand.new()
+			@txt += " " * ram.getRand
+			if @txt.length <= 140 then
+				
+			elsif @txt.length >=140 then
+				@txt.gsub(140..@txt.length,"")
+			end
 		end
 	end
 
@@ -53,32 +72,47 @@ class Tweet
 		@txt
 	end
 
-end
-
-class Rand
-	def initialize()
-		@Rand  = rand(1..8)
 	end
-	def getRand
-		@Rand
+
+	class Rand
+		def initialize()
+			@Rand  = rand(1..8)
+		end
+		def getRand
+			@Rand
+		end
 	end
-end
 
 
 
 
 
-include Clockwork
-every(300.minutes, 'nissy') do
-  
-  r = Rand.new()
-  t = Tweet.new(CK,CS,AT,ATC)
-  t.setData(r.getRand)
-  puts t.getTxt
-  t.getClient.update(t.getTxt)
-  sleep(6000)
-  t.setData(0)
-  puts t.getTxt
-  t.getClient.update(t.getTxt)
+	include Clockwork
+	every(10.minutes, 'nissy') do
 
-end
+		r = Rand.new()
+		t = Tweet.new(CK,CS,AT,ATC)
+
+		t.setData(r.getRand,"nissy")
+		puts t.getTxt
+		t.getClient.update(t.getTxt)
+
+		sleep(6000)
+
+		t.setData(r.getRand,"AAA")
+		puts t.getTxt
+		t.getClient.update(t.getTxt)
+
+		sleep(6000)
+
+		t.setData(0,"nissy")
+		puts t.getTxt
+		t.getClient.update(t.getTxt)
+
+		sleep(6000)
+
+		t.setData(0,"AAA")
+		puts t.getTxt
+		t.getClient.update(t.getTxt)
+
+	end
